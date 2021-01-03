5312c5a883ff4c7c65308d2448c9a6ab13162425
Corey <aingc@users.noreply.github.com>
2021-01-03 15:16:50 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  //cannot put hooks in conditional, in functions, or loops
+  //[CURRENT_STATE, FUNCTION_TO_UPDATE_CURRENT_STATE]
+  const [count, setCount] = useState(4);
+  const [theme, setTheme] = useState('blue');
+
+  //const [count, setCount] = useState(() => foo) will only get called one time vs
+  //const [count, setCount] = useState(foo()) will get called everytime the state changes
+
+  const decrementCount = () => {
+    setCount(prevCount => prevCount - 1);
+    //if done with just count - 1, if you run it twice, it will keep using original value for count and it won't update more than once
+    //so if you call with count - 1 twice, it will be 4 - 1 for both calls
+  }
+
+  const incrementCount = () => {
+    setCount(prevCount => prevCount + 1);
+    setTheme('red');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <span>{theme}</span>
+      <button onClick={incrementCount}>+</button>
+    </>
   );
 }
 
